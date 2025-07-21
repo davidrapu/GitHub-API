3b1edf9e813c1f6ec761f46045a74c7cb99122c4
davidrapu <168489960+davidrapu@users.noreply.github.com>
2025-07-21 23:37:42 +0100

    Basic functionality of the app complete. all that is left is to make it better when react is learnt

--- script.js
+++ script.js
@@ -1,41 +1,55 @@
 async function fetchUserData(username) {
+    let userData = undefined;
     await fetch(`https://api.github.com/users/${username}`).then(response => {
         console.log("User data fetched successfully");
         return response.json();
     }).catch(error =>{
         console.error('Error fetching user data:', error);
         return false;
+    }).then(data => {
+        userData = data;
     });
+    return userData;
 }
-
-async function fetchUrlData(url){
-    await fetch(url).then(response => {
-        return response.json();
-    }).catch(error =>{
-        console.error('Error fetching URL data:', error);
-        return false
-    });
-}
-
 async function displayUserData(username){
     const box = document.querySelector('.box');
     const userData = await fetchUserData(username);
 
     if (userData === false) return false;
+    if (userData === undefined) return false;
+    document.querySelector(".display").classList.remove("hidden")
+    console.log(userData);
 
     const userName = userData.name;
     const userImage = userData.avatar_url;
     const userFollowersCount = userData.followers;
     const userFollowingCount = userData.following;
+    const displayContainer = document.querySelector(".display");
+    displayContainer.innerHTML = '';
 
-    console.log(userName);
+    const html = `
+            <div class="imagecontainer">
+                <img src="${userImage}" alt="user_image">
+            </div>
+            <div class="description">
+                <p>Name: ${userName}</p><p>Followers: ${userFollowersCount}</p><p>Followings: ${userFollowingCount}</p>
+            </div>
+    `;
+    displayContainer.insertAdjacentHTML("afterbegin", html);
+    return true;
 }
 
-document.getElementById("username_search_form").addEventListener('submit', (e) => {
+document.getElementById("username_search_form").addEventListener('submit', async (e) => {
     e.preventDefault();
     const form = new FormData(e.target);
 
     // form object
     const data = Object.fromEntries(form.entries());
-    console.log(data.username_input);
+    const userDisplayed = await displayUserData(data.username_input.trim());
+
+    if (userDisplayed === true) {
+        e.target.querySelector('[name="username_input"]').value = '';
+    }else{
+        alert("Username is Invalid!!!");
+    }
 })
